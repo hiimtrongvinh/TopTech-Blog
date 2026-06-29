fe364bbad36225f0adbbcd572a57a16943217d79
Nguyen Trong Vinh <hiimtrongvinh@gmail.com>
2026-06-29 16:06:08 +0700

    feat: setup cross-environment asset routing for local and github pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/TopTech-Blog/', // Correct path format for GitHub Pages (starts and ends with slash)
+  base: process.env.NODE_ENV === 'production' ? '/TopTech-Blog/' : '/', // Correct path format for GitHub Pages (starts and ends with slash)
 });
