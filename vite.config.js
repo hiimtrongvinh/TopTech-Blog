4fca17f01b7746e84c7b0470b1514a8764284a5a
Nguyen Trong Vinh <hiimtrongvinh@gmail.com>
2026-06-29 16:11:16 +0700

    fix: enforce vite command build base path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/TopTech-Blog/' : '/', // Correct path format for GitHub Pages (starts and ends with slash)
-});
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/TopTech-Blog/' : '/',
+  }
+})
